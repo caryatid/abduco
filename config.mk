3a23ea0d5fd361eecd335f274d2abae01af98882
David Setchell <david.setchell@nwea.org>
2017-05-19 14:11:33 -0700

    foo

--- config.mk
+++ config.mk
@@ -10,8 +10,8 @@ INCS = -I.
 LIBS = -lc -lutil
 
 CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700
-CFLAGS += -std=c99 -pedantic -Wall ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG ${CPPFLAGS}
-LDFLAGS += ${LIBS}
+CFLAGS += -I../musl/include -std=c99 -pedantic -Wall ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG ${CPPFLAGS}
+LDFLAGS += -L../musl/lib ${LIBS} -static
 
 DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb
 
